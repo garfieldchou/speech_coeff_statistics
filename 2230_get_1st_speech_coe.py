674ae52e055d0a2dd1a21517c325f6514f28e30a
shaudai <shaudai0402@hotmail.com>
2015-06-19 10:58:08 +0800

    Open default file if no input filename.

--- 2230_get_1st_speech_coe.py
+++ 2230_get_1st_speech_coe.py
@@ -1,11 +1,15 @@
 fn = raw_input('Which speech coefficient would you like to extract? :')
-# put default file name here
-fd = open(fn)
+if len(fn) == 0: fn = '2g_speech_coeff.txt'
+try:
+    fd = open(fn)
+except:
+    print "File doesn't exist"
+    exit()
 fd_out = open('extract.txt', 'w')
 
 num_of_line = 1
 for line in fd:
-    if (num_of_line%4) == 2:
+    if (num_of_line%4) == 2: # Extract 2nd line of a frame
         fd_out.write(line)
     num_of_line = num_of_line+1
 # print num_of_line
