5be215eb65a233b0675aa360565acd2a875b17ee
shaudai <shaudai0402@hotmail.com>
2015-06-19 11:49:10 +0800

    Use a dictionary to count the codec types

--- 2230_get_1st_speech_coe.py
+++ 2230_get_1st_speech_coe.py
@@ -1,5 +1,5 @@
 fn = raw_input('Which speech coefficient would you like to extract? :')
-if len(fn) == 0: fn = '2g_speech_coeff.txt'
+if len(fn) == 0: fn = 'simplified_2g_speech_coeff.txt'
 try:
     fd = open(fn)
 except:
@@ -8,12 +8,16 @@ except:
 fd_out = open('extract.txt', 'w')
 
 num_of_line = 1
+codec_type_counts = dict()
 for line in fd:
     if (num_of_line%4) == 2: # Extract 2nd line of a frame
         fd_out.write(line)
+        # create a dictionary to count codec type here
+        codec_type_counts[line[1]] = codec_type_counts.get(line[1],0)+1
+        # print codec_type_counts
     num_of_line = num_of_line+1
 # print num_of_line
-
+print codec_type_counts
 #for i in range(num_of_line/4 +1):
 # for i in range(5):
     # print i
