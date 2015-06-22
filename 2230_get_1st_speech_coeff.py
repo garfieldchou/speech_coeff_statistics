fe7178da4029dc6b034c34dd3c41484b25ae28d4
garfield_chou <garfield_chou@asus.com>
2015-06-22 15:12:05 +0800

    1. Add frame number at the beginning of every output line 2. Remove unnecessary comments

--- 2230_get_1st_speech_coeff.py
+++ 2230_get_1st_speech_coeff.py
@@ -7,10 +7,12 @@ except:
     exit()
 fd_out = open('extract.txt', 'w')
 
-num_of_line = 1
+line_cursor = 1
 codec_type_counts = dict()
 for line in fd:
-    if (num_of_line%4) == 2: # Extract 2nd line of a frame
+    if (line_cursor%4) == 1: # Record the frame number
+        fd_out.write(line.split()[0])
+    if (line_cursor%4) == 2: # Extract 2nd line of a frame
         fd_out.write(line)
         # create a dictionary to count codec type here
         if '3' == line[1]:
@@ -23,13 +25,7 @@ for line in fd:
             codec_type_counts['HR'] = codec_type_counts.get('HR',0)+1
         else:
             codec_type_counts[line[1]] = codec_type_counts.get(line[1],0)+1
-        # print codec_type_counts
-    num_of_line = num_of_line+1
-# print num_of_line
+    line_cursor = line_cursor + 1
 print codec_type_counts
-#for i in range(num_of_line/4 +1):
-# for i in range(5):
-    # print i
-    # fd_out.write('hello\n')
 fd_out.close()
 print "Please checkout the extraction file, 'extract.txt'."
