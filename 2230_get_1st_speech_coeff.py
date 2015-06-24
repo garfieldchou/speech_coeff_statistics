bf78f7d6b066e819630c0878e5ad4b8b2bc9823f
garfield_chou <garfield_chou@asus.com>
2015-06-24 17:51:25 +0800

    Add RX_TYPE for AMR-NB and print them line by line

--- 2230_get_1st_speech_coeff.py
+++ 2230_get_1st_speech_coeff.py
@@ -1,5 +1,5 @@
 fn = raw_input('Which speech coefficient would you like to extract? :')
-if len(fn) == 0: fn = 'simplified_2g_speech_coeff.txt'
+if len(fn) == 0: fn = 'bplog_01_2015_0608_113311_2g3g_speech_coeff.txt'
 try:
     fd = open(fn)
 except:
@@ -9,6 +9,7 @@ fd_out = open('extract.txt', 'w')
 
 line_cursor = 1
 codec_type_counts = dict()
+amr_rx_type = dict()
 for line in fd:
     if (line_cursor%4) == 1: # Record the frame number
         fd_out.write(line.split()[0])
@@ -17,6 +18,22 @@ for line in fd:
         # create a dictionary to count codec type here
         if '3' == line[1]:
             codec_type_counts['AMR'] = codec_type_counts.get('AMR',0)+1
+            if '0' == line[5]:
+                amr_rx_type['RX_SPEECH_GOOD'] = amr_rx_type.get('RX_SPEECH_GOOD', 0)+1
+            elif '1' == line[5]:
+                amr_rx_type['RX_SPEECH_DEGRADED'] = amr_rx_type.get('RX_SPEECH_DEGRADED', 0)+1
+            elif '2' == line[5]:
+                amr_rx_type['RX_ONSET'] = amr_rx_type.get('RX_ONSET', 0)+1
+            elif '3' == line[5]:
+                amr_rx_type['RX_SPEECH_BAD'] = amr_rx_type.get('RX_SPEECH_BAD', 0)+1
+            elif '4' == line[5]:
+                amr_rx_type['RX_SID_FIRST'] = amr_rx_type.get('RX_SID_FIRST', 0)+1
+            elif '5' == line[5]:
+                amr_rx_type['RX_SID_UPDATE'] = amr_rx_type.get('RX_SID_UPDATE', 0)+1
+            elif '6' == line[5]:
+                amr_rx_type['RX_SID_BAD'] = amr_rx_type.get('RX_SID_BAD', 0)+1
+            else:
+                amr_rx_type['RX_NO_DATA'] = amr_rx_type.get('RX_NO_DATA', 0)+1                
         elif '0' == line[1]:
             codec_type_counts['FR'] = codec_type_counts.get('FR',0)+1
         elif '1' == line[1]:
@@ -26,6 +43,9 @@ for line in fd:
         else:
             codec_type_counts[line[1]] = codec_type_counts.get(line[1],0)+1
     line_cursor = line_cursor + 1
-print codec_type_counts
+print "\n# of frames for every codec type: \n", codec_type_counts
+print "\nAMR RX TYPE:"
+for key, value in amr_rx_type.items():
+    print key, value
 fd_out.close()
 print "Please checkout the extraction file, 'extract.txt'."
