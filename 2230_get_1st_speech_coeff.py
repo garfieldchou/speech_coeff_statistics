3208cade7a3dadefd45a43194b26b33eea9898d4
garfield_chou <garfield_chou@asus.com>
2015-06-25 20:12:00 +0800

    Use 'list' instead of 'str' for counting AMR RX_TYPE

--- 2230_get_1st_speech_coeff.py
+++ 2230_get_1st_speech_coeff.py
@@ -20,32 +20,33 @@ for line in fd:
     if (line_cursor%4) == 2: # Extract 2nd line of a frame
         fd_out.write(line)
         # create a dictionary to count codec type here
-        if '3' == line[1]:
+        line = line.split()
+        if '3' == line[0]:
             codec_type_counts['AMR'] = codec_type_counts.get('AMR',0)+1
-            if '0' == line[5]:
+            if '0' == line[2]:
                 amr_rx_type['RX_SPEECH_GOOD'] = amr_rx_type.get('RX_SPEECH_GOOD', 0)+1
-            elif '1' == line[5]:
+            elif '1' == line[2]:
                 amr_rx_type['RX_SPEECH_DEGRADED'] = amr_rx_type.get('RX_SPEECH_DEGRADED', 0)+1
-            elif '2' == line[5]:
+            elif '2' == line[2]:
                 amr_rx_type['RX_ONSET'] = amr_rx_type.get('RX_ONSET', 0)+1
-            elif '3' == line[5]:
+            elif '3' == line[2]:
                 amr_rx_type['RX_SPEECH_BAD'] = amr_rx_type.get('RX_SPEECH_BAD', 0)+1
-            elif '4' == line[5]:
+            elif '4' == line[2]:
                 amr_rx_type['RX_SID_FIRST'] = amr_rx_type.get('RX_SID_FIRST', 0)+1
-            elif '5' == line[5]:
+            elif '5' == line[2]:
                 amr_rx_type['RX_SID_UPDATE'] = amr_rx_type.get('RX_SID_UPDATE', 0)+1
-            elif '6' == line[5]:
+            elif '6' == line[2]:
                 amr_rx_type['RX_SID_BAD'] = amr_rx_type.get('RX_SID_BAD', 0)+1
             else:
                 amr_rx_type['RX_NO_DATA'] = amr_rx_type.get('RX_NO_DATA', 0)+1                
-        elif '0' == line[1]:
+        elif '0' == line[0]:
             codec_type_counts['FR'] = codec_type_counts.get('FR',0)+1
-        elif '1' == line[1]:
+        elif '1' == line[0]:
             codec_type_counts['EFR'] = codec_type_counts.get('EFR',0)+1
-        elif '2' == line[1]:
+        elif '2' == line[0]:
             codec_type_counts['HR'] = codec_type_counts.get('HR',0)+1
         else:
-            codec_type_counts[line[1]] = codec_type_counts.get(line[1],0)+1
+            codec_type_counts[line[0]] = codec_type_counts.get(line[0],0)+1
     line_cursor = line_cursor + 1
 print "\n<# of frames for every codec type>"
 list_dict_key_value(codec_type_counts, 30)
